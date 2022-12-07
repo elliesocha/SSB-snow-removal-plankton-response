6b0012d88d22b77fe9e2dbd5b7c82df1f5a1c728
Ellie Socha <esocha2@wisc.edu>
2022-12-07 15:56:04 -0500

    ANOSIM - no difference between communities of different years (successfully created an NMDS plot though... no differences here either).

--- SSBcode/FigureX_ANOSIM_stats.R
+++ SSBcode/FigureX_ANOSIM_stats.R
@@ -53,20 +53,15 @@ orditorp(nmds,display="species",col="red",air=0.01)
 orditorp(nmds,display="sites",cex=1.25,air=0.01)
 
 treat=c(rep("Reference Year",3),rep("Manipulation Year 1",3), rep("Manipulation Year 2", 5))
+colors=c(rep("blue",3),rep("red",3), rep("orange",5))
 ordiplot(nmds,type="n")
-ordihull(nmds,groups=treat,draw="polygon",col="grey90",label=F)
-orditorp(nmds,display="species",col="red",air=0.01)
-orditorp(nmds,display="sites",col=c(rep("green",3),rep("blue",3), rep("orange",5)),
-         air=0.01,cex=1.25)
-colors=c(rep("red",3),rep("blue",3), rep("orange",5))
-ordiplot(nmds,type="n")
+orditorp(nmds,display="sites",col=c(rep("blue",3),
+                      rep("red",3), rep("orange",5)),air=0.01,cex=1.25)
 #Plot convex hulls with colors baesd on treatment
 for(i in unique(treat)) {
   ordihull(nmds$point[grep(i,treat),],draw="polygon",
            groups=treat[treat==i],col=colors[grep(i,treat)],label=F) } 
 orditorp(nmds,display="species",col="black",air=0.01)
-orditorp(nmds,display="sites",col=c(rep("red",3),
-                                            rep("blue",3), rep("orange",5)),air=0.01,cex=1.25)
 
 #~~~~~~~~~~~~~~~
 #anosim between reference year and second manipulation year(?) --> NOT SIGNIFICANT (p = 0.0538)
