9878b36860fbeed4b2bff2641d01d5de364edb46
Ellie Socha <esocha2@wisc.edu>
2022-12-13 14:22:31 -0500

    Significant for all years (taxa_count by taxa_id)

--- SSBcode/FigureX_ANOSIM_phytoplankton.R
+++ SSBcode/FigureX_ANOSIM_phytoplankton.R
@@ -236,3 +236,189 @@ for(i in unique(treat)) {
            groups=treat[treat==i],col=colors[grep(i,treat)],label=F) } 
 orditorp(nmds4,display="species",col="black",air=0.01)
 #####
+
+
+
+
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#ALL YEARS
+#####
+#ANOSIM method A
+  #ALL years, R = 0.7693, pvalue = 1e-04
+  group.df <- SSB %>%
+    select(year, sample_date) %>%
+    distinct(year, sample_date, .keep_all = TRUE)
+  #pivoting table to match abundance matrix 
+  p.pivoted <- SSB %>%
+    select(sample_date, taxa_id, taxa_count) %>%
+    group_by(sample_date, taxa_id) %>%
+    summarise(taxa_count = sum(taxa_count))  %>%
+    pivot_wider(names_from = taxa_id, values_from = taxa_count, values_fill = 0) %>%
+    ungroup() %>% select(-sample_date)
+  #converting p.pivoted df into matrix
+  p.matrix = as.matrix(p.pivoted)
+  #ANOSIM
+  ano = anosim(p.matrix, group.df$year, distance = "bray", permutations = 9999)
+  ano
+#ANOSIM method B 
+  #ALL years, R = 0.7693, pvalue = 0.001
+  p.dist <- vegdist(p.pivoted)
+  p.ano <- with(group.df, anosim(p.dist, year))
+  summary(p.ano)
+  plot(p.ano)
+#                          Call:
+#                            anosim(x = p.dist, grouping = year) 
+#                         Dissimilarity: bray 
+#                          
+#                          ANOSIM statistic R: 0.7693 
+#                          Significance: 0.001 
+#                          
+#                          Permutation: free
+#                          Number of permutations: 999
+#                          
+#                          Upper quantiles of permutations (null model):
+#                            90%   95% 97.5%   99% 
+#                            0.216 0.304 0.404 0.508 
+#                          
+#                          Dissimilarity ranks between and within classes:
+#                                 0%     25%    50%    75%  100%  N
+#                         Between  1    30.00   43.0  54.5   66   47
+#                          2019    3    8.00    13.0  15.5   18   3
+#                          2020   4      5.75    13.5  20.5   23   6
+#                          2021   2     11.25    15.5  27.5   32   10
+  
+  #NMDS attempt(?) overlapping polygons
+  nmds <- metaMDS(p.pivoted)
+  nmds
+  plot(nmds)
+  ordiplot(nmds, type="n")
+  orditorp(nmds,display="species",col="red",air=0.01)
+  orditorp(nmds,display="sites",cex=1.25,air=0.01)
+  
+  treat=c(rep("Reference Year",3),rep("Manipulation Year 1",3), rep("Manipulation Year 2", 5))
+  colors=c(rep("blue",3),rep("red",3), rep("orange",5))
+  ordiplot(nmds,type="n")
+  orditorp(nmds,display="sites",col=c(rep("blue",3),
+                                      rep("red",3), rep("orange",5)),air=0.01,cex=1.25)
+  #Plot convex hulls with colors baesd on treatment
+  for(i in unique(treat)) {
+    ordihull(nmds$point[grep(i,treat),],draw="polygon",
+             groups=treat[treat==i],col=colors[grep(i,treat)],label=F) } 
+  orditorp(nmds,display="species",col="black",air=0.01)
+#####
+
+#2019 and 2021
+#####
+  #ANOSIM method A
+  #Refernce and Manipulation 2, R = 1 (?), pvalue = 0.017
+  SSB2 <- SSB %>%
+    select(sample_date, taxa_id, taxa_count) %>%
+    filter(sample_date < '2020-01-01' | sample_date > '2020-12-31') %>%
+    group_by(sample_date, taxa_id) %>%
+    summarise(taxa_count = sum(taxa_count)) %>%
+    pivot_wider(names_from = taxa_id, values_from = taxa_count, values_fill = 0) %>%
+    ungroup() %>% select(-sample_date)
+  #dataframe with grouping info (sample year and date) to be used in ANOSIM analysis
+  group.df2 <- SSB %>%
+    select(year, sample_date) %>%
+    distinct(year, sample_date, .keep_all = TRUE) %>%
+    filter(sample_date < '2020-01-01' | sample_date > '2020-12-31') 
+  #converting p.pivoted df into matrix
+  p.matrix2 = as.matrix(SSB2)
+  #ANOSIM
+  ano2 = anosim(p.matrix2, group.df2$year, distance = "bray", permutations = 9999)
+  ano2
+  
+  #ANOSIM method B 
+  #ALL years, R = 1 (?), pvalue = 0.016
+  p.dist2 <- vegdist(SSB2)
+  p.ano2 <- with(group.df2, anosim(p.dist2, year))
+  summary(p.ano2)
+  plot(p.ano2)
+  
+  #NMDS attempt overlapping polygons
+  nmds2 <- metaMDS(SSB2)
+  nmds2
+  plot(nmds2)
+  ordiplot(nmds2, type="n")
+  orditorp(nmds2,display="species",col="red",air=0.01)
+  orditorp(nmds2,display="sites",cex=1.25,air=0.01)
+  
+  treat=c(rep("Reference Year",3), rep("Manipulation Year 2", 5))
+  colors=c(rep("blue",3), rep("orange",5))
+  ordiplot(nmds2,type="n")
+  orditorp(nmds2,display="sites",col=c(rep("blue",3),
+                                       rep("orange",5)),air=0.01,cex=1.25)
+  #Plot convex hulls with colors baesd on treatment
+  for(i in unique(treat)) {
+    ordihull(nmds2$point[grep(i,treat),],draw="polygon",
+             groups=treat[treat==i],col=colors[grep(i,treat)],label=F) } 
+  orditorp(nmds2,display="species",col="black",air=0.01)
+  #####
+
+  
+#2020 and 2021
+#####
+  #ANOSIM method A
+  #Refernce and Manipulation 2, R = 0.9937, pvalue = 0.0084
+  
+  SSB3 <- SSB %>%
+    select(sample_date, taxa_id, taxa_count) %>%
+    filter(sample_date > '2019-12-31') %>%
+    group_by(sample_date, taxa_id) %>%
+    summarise(taxa_count = sum(taxa_count)) %>%
+    pivot_wider(names_from = taxa_id, values_from = taxa_count, values_fill = 0) %>%
+    ungroup() %>% select(-sample_date)
+  #dataframe with grouping info (sample year and date) to be used in ANOSIM analysis
+  group.df3 <- SSB %>%
+    select(year, sample_date) %>%
+    distinct(year, sample_date, .keep_all = TRUE) %>%
+    filter(sample_date > '2019-12-31') 
+  #converting p.pivoted df into matrix
+  p.matrix3 = as.matrix(SSB3)
+  #ANOSIM
+  ano3 = anosim(p.matrix3, group.df3$year, distance = "bray", permutations = 9999)
+  ano3
+  
+  #ANOSIM method B 
+  #ALL years, R = 0.9937, pvalue = 0.01
+  p.dist3 <- vegdist(SSB3)
+  p.ano3 <- with(group.df3, anosim(p.dist3, year))
+  summary(p.ano3)
+  plot(p.ano3)
+  #####
+
+  
+#2020 and 2021
+#####
+  #ANOSIM method A
+  #Refernce and Manipulation 2, R = 0.3333, pvalue = 0.1429
+  SSB4 <- SSB %>%
+    select(sample_date, taxa_id, taxa_count) %>%
+    filter(sample_date < '2021-01-01') %>%
+    group_by(sample_date, taxa_id) %>%
+    summarise(taxa_count = sum(taxa_count)) %>%
+    pivot_wider(names_from = taxa_id, values_from = taxa_count, values_fill = 0) %>%
+    ungroup() %>% select(-sample_date)
+  #dataframe with grouping info (sample year and date) to be used in ANOSIM analysis
+  group.df4 <- SSB %>%
+    select(year, sample_date) %>%
+    distinct(year, sample_date, .keep_all = TRUE) %>%
+    filter(sample_date < '2021-01-01') 
+  #converting p.pivoted df into matrix
+  p.matrix4 = as.matrix(SSB4)
+  #ANOSIM
+  ano4 = anosim(p.matrix4, group.df4$year, distance = "bray", permutations = 9999)
+  ano4
+  
+  #ANOSIM method B 
+  #ALL years, R = 0.3333, pvalue = 0.135
+  p.dist4 <- vegdist(SSB4)
+  p.ano4 <- with(group.df4, anosim(p.dist4, year))
+  summary(p.ano4)
+  plot(p.ano4)
+
+
